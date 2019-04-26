fb3d7869eb688c1012363b65b59d9c76fcf5f765
Midor2Mid <14520777@gm.uit.edu.vn>
2019-04-26 16:31:19 +0700

    Buổi 3: Types và Data

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Learning GraphQL
 const express = require('@feathersjs/express');
 const app = express(feathers());;`
   * Trong package.json thêm: "@feathersjs/express": "^1.2.3", và npm i
- # Buổi 2: Use graphQL
+# Buổi 2: Use graphQL
 * npm install --save express-graphql graphql
   * express-graphql: GraphQL package can be used as a middleware in express, allow us to point at schema at resolvers and automatically connect all of that, route requests to a parser and handle them according to schema and forward them to the resolvers.
   * graphql: allow us to define the schema and setup a schema that follows the official graphQL specification and definition that will give us a valid schema. Parse our schema and convert it and we can then use this parsed.
--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const feathers = require('@feathersjs/feathers');
 const bodyParser = require('body-parser');
 const express = require('@feathersjs/express');
 const graphQLHttp = require('express-graphql');
-const  { buildSchema }= require('graphql');
+const { buildSchema } = require('graphql');
 
-const app = express(feathers());;
+const app = express(feathers());
+
+const events = [];
 
 app.use(bodyParser.json());
 
@@ -14,12 +16,27 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphQLHttp({
     schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    input  EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+    }
+
     type RootQuery {
-        events:  [String!]!
+        events:  [Event!]!
     }
 
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -27,13 +44,22 @@ app.use('/graphql', graphQLHttp({
         mutation: RootMutation
     }
     `),
-    rootValue: { 
+    rootValue: {
         events: () => {
-            return ["Romantic Cooking", "Sailing", "All-Night Coding"]
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            // const eventName = args.name;
+            // return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event);
+            return event
         }
     },
     graphiql: true
